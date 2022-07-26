4a3183882d8b1b18c73fe33cc5b176d3044b281b
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-07-26 16:37:21 +0700

    Thêm nút chọn kiểu hiển thị

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardTitle, CardBody } from "reactstrap";
 import RenderStaff from "./RenderStaffComponent";
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 class StaffList extends Component {
   constructor(props) {
@@ -8,11 +14,20 @@ class StaffList extends Component {
     this.state = {
       staffSelected: null,
       columDefault: "col-12 col-md-6 col-lg-3 mt-3",
+      dropdownOpen: false,
     };
+    this.toggle = this.toggle.bind(this);
+    this.handleStaffSelected = this.handleStaffSelected.bind(this);
   }
 
   handleStaffSelected(staff) {
     this.setState({ staffSelected: staff });
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
   }
 
   render() {
@@ -34,7 +49,29 @@ class StaffList extends Component {
     });
 
     return (
-      <div>
+      <div className="container">
+        <div className="row mt-3 mb-2">
+          <div className="col-9">
+            <h5>Bấm vào nhân viên để xem thông tin chi tiết</h5>
+          </div>
+          <div className="col-2 ">
+            <ButtonDropdown
+              isOpen={this.state.dropdownOpen}
+              toggle={this.toggle}
+              className="ml-5"
+            >
+              <DropdownToggle caret color="danger">
+                Kiểu hiển thị
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>6 cột</DropdownItem>
+                <DropdownItem>4 cột</DropdownItem>
+                <DropdownItem>3 cột</DropdownItem>
+                <DropdownItem>2 cột</DropdownItem>
+              </DropdownMenu>
+            </ButtonDropdown>
+          </div>
+        </div>
         <RenderStaff
           staff={this.state.staffSelected}
           handleStaffSelected={this.handleStaffSelected}
