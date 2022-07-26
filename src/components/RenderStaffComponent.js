99adf2452a0af32526735026e0e54782df8ff15c
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-07-26 23:14:46 +0700

    Chỉnh sửa hiển thị chi tiết nhân viên

--- src/components/RenderStaffComponent.js
+++ src/components/RenderStaffComponent.js
@@ -2,29 +2,24 @@ import React from "react";
 import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
 import dateFormat from "dateformat";
 
-function RenderImg({ staff }) {
-  return (
-    <div>
-      <Card>
-        <CardImg src={staff.image} alt={staff.name} />
-      </Card>
-    </div>
-  );
-}
-
 function RenderDetails({ staff }) {
   return (
-    <div>
-      <CardBody>
-        <CardTitle>Họ và tên: {staff.name}</CardTitle>
-        <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
-        <CardText>
-          Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-        </CardText>
-        <CardText>Phòng ban: {staff.department.name}</CardText>
-        <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-      </CardBody>
+    <div className="row">
+      <div className="col-12 col-md-5 m-1">
+        <CardImg src={staff.image} alt={staff.name} />
+      </div>
+      <div className="col-12 col-md-6 m-1">
+        <CardBody>
+          <CardTitle>Họ và tên: {staff.name}</CardTitle>
+          <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+          <CardText>
+            Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+          </CardText>
+          <CardText>Phòng ban: {staff.department.name}</CardText>
+          <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+          <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+        </CardBody>
+      </div>
     </div>
   );
 }
@@ -32,24 +27,19 @@ function RenderDetails({ staff }) {
 const RenderStaff = (props) => {
   if (props.staff) {
     return (
-      <Card className="m-2">
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <div>
-              <RenderImg staff={props.staff} />
-              <RenderDetails staff={props.staff} />
-            </div>
-            <button
-              type="button"
-              class="close"
-              aria-label="Close"
-              onClick={() => props.handleStaffSelected(null)}
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
+      <div className="row border border-success rounded mt-3 mb-2">
+        <RenderDetails staff={props.staff} />
+        <div className="col-12 col-md-5 m-1">
+          <button
+            type="button"
+            class="close"
+            aria-label="Close"
+            onClick={() => props.handleStaffSelected(null)}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-      </Card>
+      </div>
     );
   } else return <div></div>;
 };
