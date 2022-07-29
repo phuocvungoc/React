a39e097d43e7d482ec588683737b158f28a1d767
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-07-29 10:33:04 +0700

    Use function component

--- src/components/PhongBan.js
+++ src/components/PhongBan.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardTitle, CardBody, CardText } from "reactstrap";
 
-class PhongBan extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const phongBanList = this.props.departments.map((department) => {
-      return (
-        <div className="col-12 col-lg-4 col-md-6 mt-3" key={department.id}>
-          <Card>
-            <CardBody className="border border-success rounded">
-              <CardTitle>{department.name}</CardTitle>
-              <CardText>
-                Số lượng nhân viên: {department.numberOfStaff}
-              </CardText>
-            </CardBody>
-          </Card>
-        </div>
-      );
-    });
+function RenderPB({ department }) {
+  return (
+    <Card>
+      <CardBody className="border border-success rounded">
+        <CardTitle>{department.name}</CardTitle>
+        <CardText>Số lượng nhân viên: {department.numberOfStaff}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+const PhongBan = (props) => {
+  const phongBanList = props.departments.map((department) => {
     return (
-      <div className="container">
-        <div className="row">{phongBanList}</div>
+      <div className="col-12 col-lg-4 col-md-6 mt-3" key={department.id}>
+        <RenderPB department={department} />
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="container">
+      <div className="row">{phongBanList}</div>
+    </div>
+  );
+};
 
 export default PhongBan;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,38 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardTitle, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
 
-class StaffList extends Component {
-  constructor(props) {
-    super(props);
-  }
+function RenderListStaff({ staff }) {
+  return (
+    <Card>
+      <CardBody className="border border-success rounded">
+        <Link to={`/nhanvien/${staff.id}`}>
+          <CardImg className="rounded" src={staff.image} alt={staff.name} />
+        </Link>
+        <CardTitle className="text-center mt-4">{staff.name}</CardTitle>
+      </CardBody>
+    </Card>
+  );
+}
 
-  render() {
-    const staffList = this.props.staffs.map((staff) => {
-      return (
-        <div className="col-12 col-md-6 col-lg-3 mt-3" key={staff.id}>
-          <Card>
-            <CardBody className="border border-success rounded">
-              <Link to={`/nhanvien/${staff.id}`}>
-                <CardImg
-                  className="rounded"
-                  src={staff.image}
-                  alt={staff.name}
-                />
-              </Link>
-              <CardTitle className="text-center mt-4">{staff.name}</CardTitle>
-            </CardBody>
-          </Card>
-        </div>
-      );
-    });
-
+const StaffList = (props) => {
+  const staffList = props.staffs.map((staff) => {
     return (
-      <div className="container">
-        <div className="row">{staffList}</div>
+      <div className="col-12 col-md-6 col-lg-3 mt-3" key={staff.id}>
+        <RenderListStaff staff={staff} />
       </div>
     );
-  }
-}
+  });
+  return (
+    <div className="container">
+      <div className="row">{staffList}</div>
+    </div>
+  );
+};
 
 export default StaffList;
